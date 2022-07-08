ee833682510cb3e1ba978a35f4b63ee509f1f871
Julia-Harding <90004272+Julia-Harding@users.noreply.github.com>
2022-07-07 15:19:10 -1000

    Add files via upload

---
+++ scripts/script.js
@@ -0,0 +1,83 @@
+/*
+	Bridgette Goss
+	script.js
+	11/10/2021
+*/
+
+//Global Variables
+var video = document.getElementById("example");
+var videoSource = document.getElementById("vid-src");
+var descriptionSource = document.getElementById("despsrc");
+
+//Hamburger menu function
+function hamburger() {
+	var menu = document.getElementById("menu-links");
+	var logo = document.getElementById("ffc-logo");
+	if (menu.style.display === "block" && logo.style.display === "none") {
+		menu.style.display = "none";
+		logo.style.display = "block";
+	} else {
+		menu.style.display = "block";
+		logo.style.display = "none";
+	}
+}
+var slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+}
+
+//Function to display the burpee example video
+function burpees() {
+	videoSource.src = "media/burpees.mp4";
+	descriptionSource.src = "media/burpees-descriptions.vtt";
+	video.style.display = "block";
+	video.load();
+}
+
+//Function to display plank video
+function plank() {
+	videoSource.src = "media/plank.mp4";
+	descriptionSource.src = "media/plank-descriptions.vtt";
+	video.style.display = "block";
+	video.load();
+}
+
+//Function to display mountain climber video
+function mountain() {
+	videoSource.src = "media/mc.mp4";
+	descriptionSource.src = "media/mountain-descriptions.vtt";
+	video.style.display = "block";
+	video.load();
+}
+
+//Function to display a promo code
+function discount() {
+	var promo = document.getElementById("special");
+	promo.firstChild.nodeValue = "Promo Code: D25START";
+	promo.style.color = "#ff0000";
+	promo.style.fontSize = "2em";
+}
